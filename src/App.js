457d09f17cf534a5dff9c22a6b8f3d65b5bb4d0f
Martín Cortés <martinxcortes@gmail.com>
2022-03-16 09:11:24 -0500

    init

--- src/App.js
+++ src/App.js
@@ -3,6 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let installPrompt = null;
+  let buttonInstall;
+  React.useEffect(() => {
+    buttonInstall = document.getElementById('install');
+
+    window.addEventListener('beforeinstallprompt', e => {
+      e.preventDefault();
+      console.log("Install Prompt fired");
+      installPrompt = e;
+      // See if the app is already installed, in that case, do nothing
+      if ((window.matchMedia && window.matchMedia('(display-mode: standalone)').matches) || window.navigator.standalone === true) {
+        console.log('already installed');
+        return false;
+      }
+
+      buttonInstall.style.display = 'block'
+      buttonInstall.scrollIntoView()
+    });
+
+    window.addEventListener('appinstalled', e => {
+      console.log('app installed');
+    });
+
+  }, []);
+
+  function handleClick() {
+    installPrompt.prompt();
+    installPrompt.userChoice.then((choice) => {
+      if (choice.outcome === 'accepted') {
+        console.log('user accepted');
+      } else {
+        console.log('user rejected');
+      }
+      installPrompt = null;
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +47,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button id="install" onClick={handleClick}>Instalar App</button>
       </header>
     </div>
   );
